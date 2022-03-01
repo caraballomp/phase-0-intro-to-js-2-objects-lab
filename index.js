497b42a0844c9823cb0716dd039f474ca638f929
Michelle Caraballo <caraballomp@yahoo.com>
2022-02-28 20:28:48 -0800

    commit tree 4475d36756f952c73c78065f5982bc647bf1a52b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Michelle',
+    streetAddress: '123 Walk',
+}
+ const employee2 ={
+     name: 'Matt',
+     streetAddress: '987 Drive',
+ }
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newemployee = {...object}; 
+    newemployee[key] = value; 
+    return newemployee;
+}
+console.log(updateEmployeeWithKeyAndValue(employee2, 'streetAddress', '987 Drive'));
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;  
+}
+function deleteFromEmployeeByKey(object, key, value){
+    return {
+        ...object,
+        [key]: value,
+}}
+delete object.key;
+
+function destructivelyDeleteFromEmployeeByKey(object, key, value){
+        object[key] = value;
+        return object; 
+}
